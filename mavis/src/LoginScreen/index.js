c4f18f1804a00d0131f541cd222ad7970d1fc0b2
João Igor <igor.joao@academico.ifpb.edu.br>
2024-11-10 18:51:05 -0300

    style: estilizar tela de login

--- mavis/src/LoginScreen/index.js
+++ mavis/src/LoginScreen/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { Input, Button, Text, Block } from 'galio-framework';
 
 export default function LoginScreen() {
@@ -6,14 +7,15 @@ export default function LoginScreen() {
   const [password, setPassword] = useState('');
 
   return (
-    <Block safe flex>
-      <Text h4>Entrar</Text>
+    <Block safe flex style={styles.container}>
+      <Text h4 style={styles.title}>Entrar</Text>
 
       <Input
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
         rounded
+        style={styles.input}
         keyboardType="email-address"
         autoCapitalize="none"
       />
@@ -23,20 +25,51 @@ export default function LoginScreen() {
         value={password}
         onChangeText={setPassword}
         rounded
+        style={styles.input}
         password
         viewPass
       />
 
-      <Button color="info" round>
+      <Button color="info" round style={styles.button} >
         Entrar
       </Button>
 
-      <Text>
+      <Text style={styles.registerText}>
         Não tem uma conta?{' '}
-        <Text color="info">
+        <Text color="info" style={styles.link}>
           Registrar-se
         </Text>
       </Text>
     </Block>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center', 
+    backgroundColor: '#fff',
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    marginBottom: 12,
+    width: '100%', 
+  },
+  button: {
+    marginTop: 20,
+    alignSelf: 'center',
+    width: '50%',
+  },
+  registerText: {
+    textAlign: 'center',
+    marginTop: 16,
+  },
+  link: {
+    color: '#007bff',
+    textDecorationLine: 'underline',
+  }
+});
