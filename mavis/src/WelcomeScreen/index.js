7d7531c2c51feb8af556569ad46ed8b72bb7bfdc
João Igor <igor.joao@academico.ifpb.edu.br>
2025-02-28 19:45:19 -0300

    style(WelcomeScreen): estilizar tela com nativewind/tailwind

--- mavis/src/WelcomeScreen/index.js
+++ mavis/src/WelcomeScreen/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { Block, Text, Button } from 'galio-framework';
-import styles from './styles';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function WelcomeScreen({ navigation }) {
   return (
-    <Block safe flex style={styles.container}>
-      <Text h4 bold style={styles.title}>
-        Bem-vindo(a) ao MAVIS!
-      </Text>
-      <Text p style={styles.description}>
+    <View className="flex-1 justify-center items-center bg-gray-100 p-5">
+      <Text className="text-2xl font-bold text-gray-900 text-center mb-5">Bem-vindo(a) ao MAVIS!</Text>
+      <Text className="text-base text-gray-700 text-center mb-10 px-4">
         Cuidando de quem mora sozinho. O MAVIS ajuda a garantir a sua segurança
         e tranquilidade ao conectar você com seus familiares em momentos de
         necessidade. Configure seu perfil e comece a aproveitar o suporte que
         você merece.
       </Text>
-      <Button
-        color="info"
-        round
-        style={styles.button}
-        onPress={() => navigation.navigate('HomeScreen')}
-      >
-        Iniciar
-      </Button>
-    </Block>
+      <TouchableOpacity 
+        className="w-full bg-blue-900 py-3 rounded-lg items-center" 
+        onPress={() => navigation.navigate('HomeScreen')}>
+        <Text className="text-white text-lg font-bold">Iniciar</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
